8ea86a4741e18389c412b9c21f37fde4152cf975
XXX <zhang.xi6@northeastern.edu>
2026-01-19 12:53:00 -0500

    Implement kNN imputation functions for NA handling
    Added functions for kNN imputation including distance matrix initialization, finding best k, and performing imputation on features with NA values.

--- R/data_cleaning.R
+++ R/data_cleaning.R
@@ -193,3 +193,193 @@ check_multicollinearity<-function(variables_df, threshold=0.8) {
     cat("There is no multicolineariality detect in this data set!")
   }
 }
+
+#' Initialize distance matrix for kNN imputation
+#'
+#' @param df Data frame that need to be treated
+#' @return The euclidean distance list that will be used
+#' @export
+initialize_distance_find_best_k<-function(df){
+  
+  # Find all numerical column
+  numerical_columns<-names(df)[sapply(df,is.numeric)]
+  # Find all numerical col with NA
+  excluded_columns<-names(df)[colSums(is.na(df))>0]
+  # Exlude column with NA
+  target_columns<-setdiff(numerical_columns,excluded_columns)
+  # Normalization & calculate euclidean distance
+  df_matrix<-as.matrix(df[,target_columns])
+  df_matrix<-scale(df[, target_columns])
+  df_distance<-as.matrix(dist(df_matrix, method="euclidean"))
+  return(df_distance)
+}
+
+#' Get indices of non-NA values in a feature
+#'
+#' @param target_feature Specific feature that need to find not na value
+#' @return Target_feature's all index that are not na
+#' @export
+initialize_not_na_index<-function(target_feature){
+    
+    # Find index that are not na
+    not_na_index<-which(!is.na(target_feature))
+    return(not_na_index)
+}
+
+#' Sample indices to tune k for kNN imputation
+#'
+#' @param test_proportion Proportion you want to select from not_na_index to test best k
+#' @param not_na_index The not_na_index you get from previous function
+#' @return The index you wanna use to test best k
+#' @export
+initialize_test_k_index<-function(test_proportion,not_na_index) {
+  
+  # From index does not have na, randomly select certain portion of index 
+  test_k_index<-sample(not_na_index,size=floor(test_proportion*length(not_na_index)))
+  return(test_k_index)
+}
+
+#' Find best k for kNN imputation based on RMSE
+#'
+#' @param max_k Number of k you wanna test
+#' @param test_k_index The index you wanna use to test best k
+#' @param df_distance The euclidean distance matrix build before
+#' @param not_na_index The not_na_index you get from previous function
+#' @param target_feature The feature need to find best k
+#' @return Smallest_k used for kNN imputation
+#' @export
+find_best_k<-function(max_k,test_k_index,df_distance,not_na_index,target_feature){
+  
+  # Initialize
+  smallest_RMSE<-Inf
+  smallest_k<-Inf
+  
+  
+  # We gonna test k from 1 to 20
+  for (k in 1:max_k) {
+  
+    # Initialization for total standard error
+    se_total<-0
+
+    # loop over all test k index that randomly selected earlier
+    for (j in test_k_index){
+      
+      # Initialize
+      df_temp<-df_distance
+      # Set self to Inf so it won't be considered in smallest euclidean distance 
+      df_temp[j,j]<-Inf
+    
+      # Get all distance of this specific index
+      total_distance<-df_temp[j,not_na_index]
+    
+      # Ordered them the distance
+      ordered_dist<-order(total_distance)
+    
+      # Choose the closest k's distance and get their original index in original data set
+      original_index<-not_na_index[ordered_dist][1:k]
+    
+      # and calculate closest k's distance's mean value
+      # This is our prediction value
+      test_value<-round(mean(target_feature[original_index]))
+    
+      # Get actual value
+      actual_value<-target_feature[j]
+    
+      # Add up the standard error for later RMSE calculation 
+      se_total<-se_total+(actual_value-test_value)^2
+    }
+  
+    # Get RMSE value for prediction's accuracy
+    RMSE<-sqrt(se_total/length(test_k_index))
+  
+    # If this k's RMSE is smaller than any previous RMSE
+    if (RMSE < smallest_RMSE){
+    
+      # update RMSE
+      smallest_RMSE<-RMSE
+    
+      # update k
+      smallest_k<-k
+    }
+  }
+  # print out the best k we find and have its RMSE as additional value
+  cat("The best k is", smallest_k,"which give smallest error of", smallest_RMSE,"\n")
+  
+  # remove temporary vector
+  rm(df_temp,total_distance)
+  
+  # clear vector storage
+  gc()
+  
+  return(smallest_k)
+}
+
+#' Perform kNN imputation for a single feature
+#'
+#' @param df Data frame that need NA kNN imputation treatment
+#' @param smallest_k The best k calculate before
+#' @param target_feature Specific column that need NA kNN imputation
+#' @param df_distance The euclidean distance matrix made before
+#' @return Feature value that successfully imputated
+#' @export
+kNN_Imputation<-function(df,smallest_k,target_feature,df_distance){
+  # Initialization
+  k<-smallest_k
+  na_cols<-which(is.na(target_feature))
+  na_rows<-which(is.na(target_feature))
+  df_temp<-df_distance
+  
+  # For all values in target_feature
+  for (i in na_rows) {
+  
+    # Ignore self
+    df_temp[i,i]<-Inf
+  
+    # Ignore self's NA value
+    df_temp[i,na_cols]<-Inf
+  
+    # Get smallest best k euclidean distance
+    closest_k<-order(df_temp[i,])[1:k]
+  
+    # Impute the mean value of these k numbers
+    target_feature[i]<-round(mean(target_feature[closest_k]))
+  }
+  return(target_feature)
+}
+
+#' Automation kNN imputation for all columns with NA
+#'
+#' @param df The data frame that has NA need kNN imputation treatment
+#' @param test_por Proportion of the dataset used for k testing (default = 10%)
+#' @param max_k Maximum number of k test for best k (default=20)
+#' @param ignore_col Columns name that does not need its NA be treated (default=NULL)
+#' @return Data frame with designated column successfully impute
+#' @export
+automation_knn_imputation<-function(df,test_por=0.1,max_k=20,ignore_col=NULL) {
+  
+  # Collect all column name with NA
+  na_col_name<-names(df)[colSums(is.na(df))>0]
+  
+  # If there are columns want to be ignored, remove them from column name
+  if (!is.null(ignore_col)) {
+    na_col_name<-setdiff(na_col_name,ignore_col)
+  }
+  
+  # For all column's that their NA need to be impute, call functions sequentially 
+  # This function will tell you which column it is working on & how it plans to work on
+  for (i in na_col_name) {
+    target_feature<-df[[i]]
+    df_distance<-initialize_distance_find_best_k(df)
+    not_na_index<-initialize_not_na_index(target_feature)
+    test_k_index<-initialize_test_k_index(test_por,not_na_index)
+    cat("Now is processing column",i,"\n")
+    smallest_k<-find_best_k(max_k,test_k_index,df_distance,not_na_index,target_feature)
+    df[[i]]<-kNN_Imputation(df,smallest_k,target_feature,df_distance)
+    cat("Feature",i,"is successfully imputed with kNN. \n")
+    
+    # Remove unncessary information to release storage
+    rm(df_distance)
+    gc()
+  }
+  return(df)
+}
